24b6edf9ad5745ede5bb8009ebb17d98c8792fe0
Nils Dralle <nils.91@googlemail.com>
2016-08-05 18:43:25 +0200

    433 monitor
    Signed-off-by: Nils Dralle <nils.91@googlemail.com>

--- src/main.c
+++ src/main.c
@@ -6,14 +6,14 @@ int main (void)
   pinMode (1, OUTPUT) ;
   pinMode (2, INPUT) ;
   
-  digitalWrite (1, LOW) ;
+  digitalWrite (1, HIGH) ;
   pullUpDnControl (2, PUD_DOWN); 
   for (;;)
   {
 	  int r_status=digitalRead(2);
 	  digitalWrite(0, r_status);
 	  delay(250);
-	  digitalWrite(1,r_status*-1+1);
+	  
   }
   return 0 ;
 }
