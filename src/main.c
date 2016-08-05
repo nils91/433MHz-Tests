528402f30d413e15f3bbb96975089241aa5703f2
Nils Dralle <nils.91@googlemail.com>
2016-08-05 20:44:31 +0200

    edge detection
    Signed-off-by: Nils Dralle <nils.91@googlemail.com>

--- src/main.c
+++ src/main.c
@@ -1,16 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <wiringPi.h>
 int main (void)
 {
   wiringPiSetup () ;
+  //setup pins
   pinMode (0, OUTPUT) ;
   pinMode (1, OUTPUT) ;
   pinMode (2, INPUT) ;
   
   digitalWrite (1, LOW) ;
+  //pull-down on receiver input pin
   pullUpDnControl (2, PUD_DOWN); 
+  int signal_status=0;
   for (;;)
   {
 	  int r_status=digitalRead(2);
+	  if(r_status!=signal_status){
+		  printf("Signal change\n");
+		  printf("Signal is now %i\n",r_status);
+		  signal_status=r_status;
+		  
+	  }
 	  digitalWrite(0, r_status);
 	  
 	  
