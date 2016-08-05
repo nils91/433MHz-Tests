d1a3f36600274f6b0da88c174f96f61fc1b97ada
Nils Dralle <nils.91@googlemail.com>
2016-08-05 21:16:17 +0200

    crude time measurement
    Signed-off-by: Nils Dralle <nils.91@googlemail.com>

--- src/main.c
+++ src/main.c
@@ -26,19 +26,17 @@ int main (void)
   for (;;)
   {
 	  int r_status=digitalRead(2);
-	  if(r_status){
-		  signal_stop_nanosec=get_nanos();
-	  }else{
-		  signal_start_nanosec=get_nanos();
-	  }
+	  
 	  if(r_status!=signal_status){
 		  printf("Signal change\n");
 		  printf("Signal is now %i\n",r_status);
 		  if(r_status==0){
 			  printf("Signal was on %ld\n",signal_stop_nanosec-signal_start_nanosec);
+			  signal_stop_nanosec=get_nanos();
 		  }else{
 			  
 			  printf("Signal was off %ld\n",signal_start_nanosec-signal_stop_nanosec);
+			  signal_start_nanosec=get_nanos();
 		  }
 		  signal_status=r_status;
 		  
